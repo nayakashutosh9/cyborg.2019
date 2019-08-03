b40d0e2ded063f4686e231f18e4e6e61c3e8f7f6
bislara <biswajeetsahoo54@gmail.com>
2019-08-03 17:28:39 +0530

    small changes in index page

---
+++ dist/js/cert_form.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $('.btn-group').on('click', '.btn', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+});
+
+
+$(document).ready(function () {
+
+  $("#club").click(function () {
+    $("#members").css("display", "block")
+    $("#events").css("display", "none")
+    $("#projects").css("display", "none")
+  });
+
+  $("#event").click(function () {
+    $("#members").css("display", "none")
+    $("#events").css("display", "block")
+    $("#projects").css("display", "none")
+  });
+
+  $("#project").click(function () {
+    $("#members").css("display", "none")
+    $("#events").css("display", "none")
+    $("#projects").css("display", "block")
+  });
+
+});
