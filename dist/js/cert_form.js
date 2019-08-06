d25884a25b14b8a91d49e609c4046bc0738d1f67
bislara <biswajeetsahoo54@gmail.com>
2019-08-06 14:59:23 +0530

    adding one more button

--- dist/js/cert_form.js
+++ dist/js/cert_form.js
@@ -11,18 +11,29 @@ $(document).ready(function () {
     $("#members").css("display", "block")
     $("#events").css("display", "none")
     $("#projects").css("display", "none")
+    $("#temp").css("display", "none")
   });
 
   $("#event").click(function () {
     $("#members").css("display", "none")
     $("#events").css("display", "block")
     $("#projects").css("display", "none")
+    $("#temp").css("display", "none")
   });
 
   $("#project").click(function () {
     $("#members").css("display", "none")
     $("#events").css("display", "none")
     $("#projects").css("display", "block")
+    $("#temp").css("display", "none")
   });
+
+$("#project1").click(function () {
+    $("#members").css("display", "none")
+    $("#events").css("display", "none")
+    $("#projects").css("display", "none")
+    $("#temp").css("display", "block")
+  });
+
 
 });
